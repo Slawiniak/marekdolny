6d0b382ef47497b8964d965c99857dc8d8ad5aa6
Slawiniak <115100203+Slawiniak@users.noreply.github.com>
2022-11-08 12:00:32 +0100

    Update plik3.py

--- plik3.py
+++ plik3.py
@@ -1,9 +1,20 @@
-def max(lista):
-    rezultat = lista[0]
-    for element in lista:
-        if element > rezultat:
-            rezultat = element
-    return rezultat
-moja_lista = [4, 2, 5, 8, 23, 90, -3, 0, 72]
+import unittest
 
-print(max(moja_lista))
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
